6d14635412290acbeb58e973496c103c6659eee8
Douglas-Perez <douglas.perez.c@hotmail.com>
2025-03-09 03:33:42 -0300

    Criar classe Terminal e realocar o código

--- Sebank/Program.cs
+++ Sebank/Program.cs
@@ -4,57 +4,32 @@ namespace Sebank
 {
     internal class Program
     {
-        public static bool continuar = true;
-        public static int acao = 0;
-        public static bool teste= true;
-        public static float quantia;
-
-        public static ContaCorrente conta = new ContaCorrente();
-        public static void Espera()
-        {
-            Console.WriteLine("\nPressione Enter para continuar");
-            Console.ReadLine();
-            Console.Clear();
-        }
-
         static void Main(string[] args)
         {
+            ContaCorrente conta = new();
+            Terminal terminal = new();
             while (true)
             {
-                quantia = 0;
 
                 Console.WriteLine("Bem-vindo ao Sebank!\nO que gostaria de fazer?\n");
-                do
-                {
-                    Console.WriteLine("1 -> Ver saldo da conta.");
-                    Console.WriteLine("2 -> Fazer um depósito.");
-                    Console.WriteLine("3 -> Sacar um valor.");
-                    Console.WriteLine("4 -> Sair");
-                    teste = int.TryParse(Console.ReadLine(), out acao);
-                    Console.Clear();
-                    if (acao > 4 || acao < 1 || !teste)
-                    {
-                        Console.WriteLine("Por favor, escolha uma das ações abaixo:\n");
-                    }
-                } while (acao > 4 || acao < 1 || !teste);
-
-                switch (acao)
+                
+                switch (terminal.ExibirTerminal())
                 {
                     case 1:
                         conta.ExibirSaldo();
-                        Espera();
+                        terminal.Esperar();
                         break;
 
                     case 2:
                         conta.Deposito();
                         conta.ExibirSaldo();
-                        Espera();
+                        terminal.Esperar();
                         break;
 
                     case 3:
                         conta.Sacar();
                         conta.ExibirSaldo();
-                        Espera();
+                        terminal.Esperar();
                         break;
 
                     case 4:
---
+++ Sebank/Terminal.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Sebank
+{
+    class Terminal
+    {
+        public int ExibirTerminal()
+        {
+            int acao;
+            bool teste;
+            do
+            {
+                Console.WriteLine("1 -> Ver saldo da conta.");
+                Console.WriteLine("2 -> Fazer um depósito.");
+                Console.WriteLine("3 -> Sacar um valor.");
+                Console.WriteLine("4 -> Sair");
+                teste = int.TryParse(Console.ReadLine(), out acao);
+                Console.Clear();
+                if (acao > 4 || acao < 1 || !teste)
+                {
+                    Console.WriteLine("Por favor, escolha uma das ações abaixo:\n");
+                }
+            } while (acao > 4 || acao < 1 || !teste);
+            return acao;
+        }
+
+        public void Esperar()
+        {
+            Console.WriteLine("\nPressione Enter para continuar");
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+}
